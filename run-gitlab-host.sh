1681ff16d0230a0c543be598e2953d9b8a20c6a3
Aleksandr <backsapce@hotmail.com>
2020-11-17 09:37:53 +0300

    completed lab

---
+++ run-gitlab-host.sh
@@ -0,0 +1,9 @@
+sudo docker run --detach \
+  --net=host \
+  --hostname gitlab.example.com \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  gitlab/gitlab-ee:latest
